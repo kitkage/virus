ea410fe8a5efb63fe66ec8c5f3101aced5735a5b
unknown <kitkage@yahoo.com>
2013-02-10 18:19:12 -0500

    city stuff

--- src/viriussimulator/City.java
+++ src/viriussimulator/City.java
@@ -15,6 +15,16 @@ public class City extends location
     {
      nodes = new ArrayList<vNode>(); 
      numStartingAgents = numAgents; 
+     nodes.add(new streetNode("route 1", 0));
+     nodes.add(new buildingNode("building 1", numAgents/4));
+     nodes.add(new buildingNode("building 2", numAgents/4));
+     nodes.add(new buildingNode("building 3", numAgents/4));
+     nodes.add(new buildingNode("building 4", numAgents/4));
+     for (int i = 1; i < 5; i++) 
+     {
+        nodes.get(0).addConnection(nodes.get(i));
+
+      }
     }
     public  void update()
     {
